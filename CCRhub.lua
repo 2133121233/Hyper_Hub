5324d4bc609874047ed8a435c4ce5e877122d5fd
2133121233 <106486318+2133121233@users.noreply.github.com>
2023-12-06 11:42:02 -0300

    Update 2.9

--- CCRhub.lua
+++ CCRhub.lua
@@ -1,27 +1,31 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("CCR+ Hub V3")
+local PhantomForcesWindow = Library:NewWindow("CCR+ Hub V2.9")
 
 local Bladeball = PhantomForcesWindow:NewSection("BladeBall")
 
-Bladeball:CreateToggle("PARRY V1",
+Bladeball:CreateToggle("Parry V1",
 function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Parry",true))()
 end)
 
-Bladeball:CreateToggle("SPAM V1",
-function()loadstring(game:HttpGet("https://raw.githubusercontent.com/DonGabrielle/AutoDetectV4/main/MainBalls"))()
+Bladeball:CreateToggle("Spam V1", 
+function()loadstring(game:HttpGet("https://raw.githubusercontent.com/DonGabrielle/AutoDetectV4/main/MainBalls"))()  
 end)
 
-Bladeball:CreateToggle("SPAM V2",
+Bladeball:CreateToggle("Spam V2",
 function()loadstring(game:HttpGet("https://pastebin.com/raw/t2391h1A"))()
 end)
 
-Bladeball:CreateButton("REDzHUB",
+Bladeball:CreateButton("REDz BlB", 
 function()loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BladeBall/main/eng"))()
 end)
 
 Bladeball:CreateButton("DetectV1",
 function()loadstring(game:HttpGet("https://pastebin.com/raw/pjTTegEe"))()
+end)
+
+Bladeball:CreateButton("Event Parry",
+function()loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/autoparryEvent.lua"))()
 end)
 
 local Bloxf = PhantomForcesWindow:NewSection("BloxFruits")
@@ -38,6 +42,34 @@ Bloxf:CreateButton("REDz Blf",
 function()loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))() 
 end)
 
-Bloxf:CreateButton("Sky hub",
-function()loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyLoLand777/NotifyVip/main/SkyNotifyPriv.lua"))() 
+Bloxf:CreateButton("Neva",
+function()loadstring(game:HttpGet(‘https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2’))() 
+end)
+
+Bloxf:CreateButton("HOHO",
+function()loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))() 
+end)
+
+Bloxf:CreateButton("W-azure",
+function()loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))() 
+end)
+
+Bloxf:CreateButton("Fai Fao v2",
+function()loadstring(game:HttpGet"https://raw.githubusercontent.com/PNguyen0199/Script/main/Fai-Fao-Ver2.lua")() 
+end)
+
+local Brk = PhantomForcesWindow:NewSection("Brookhaven")
+
+Brk:CreateButton("Redz Bkh",
+function()loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))() 
+end)
+
+local Anf = PhantomForcesWindow:NewSection("Anime Fighters")
+
+Anf:CreateButton("Yuto",
+function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Binintrozza/yutv2e/main/afss"))() 
+end)
+
+Anf:CreateButton("HOHO Afs",
+function()loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))() 
 end)
