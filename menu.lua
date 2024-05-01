a44352053db05e65880bfb2c30c6b47c7164a45a
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-04-30 22:46:00 -0300

    Create menu.lua

---
+++ menu.lua
@@ -0,0 +1,14 @@
+local PlaceId = game.PlaceId
+if PlaceId == 13772394625 then -- Blade
+    loadstring(game:HttpGet("a"))()
+elseif PlaceId == 0000000000 then -- Vazio
+    loadstring(game:HttpGet("a"))()
+elseif PlaceId == 2753915549 then -- Blox s1
+    loadstring(game:HttpGet("a"))()
+elseif PlaceId == 4442272183 then -- Blox s2
+    loadstring(game:HttpGet("a"))()
+elseif PlaceId == 7449423635 then -- Blox s3
+    loadstring(game:HttpGet("a"))()
+elseif PlaceId == 5210095481 then -- Omni x
+    loadstring(game:HttpGet("a"))()
+end
