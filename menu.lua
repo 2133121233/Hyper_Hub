73d39a319801733bc688da89af749c64f2644b3b
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-06-06 10:32:24 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -19,7 +19,7 @@
 toclipboard("https://www.discord.gg/v8HuUR9dP8") -- or use dsc.gg/hyperscript
 local PlaceId = game.PlaceId
 if PlaceId == 13772394625 then -- Blade ball
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bladeball.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bladeballfix.lua"))()
 elseif PlaceId == 0000000000 then -- Vazio
     loadstring(game:HttpGet("a"))()
 elseif PlaceId == 2753915549 then -- Blox s1
