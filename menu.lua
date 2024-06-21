422deb6f9a61e1b23d401749301fe3b9a8362c89
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-06-21 17:22:06 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -20,17 +20,21 @@ toclipboard("https://www.discord.gg/v8HuUR9dP8")-- or use dsc.gg/hyperscript
 -- games
 local PlaceId = game.PlaceId
 if PlaceId == 13772394625 then -- Blade ball
+    repeat wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bladeballfix.lua"))()
 elseif PlaceId == 16281300371 then -- Vazio
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bladeballfix.lua"))()
+    loadstring(game:HttpGet("a"))()
 elseif PlaceId == 2753915549 then -- Blox s1
+    repeat wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bloxv2.lua"))()
 elseif PlaceId == 4442272183 then -- Blox s2
+    repeat wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bloxv2.lua"))()
 elseif PlaceId == 7449423635 then -- Blox s3
+    repeat wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bloxv2.lua"))()
 elseif PlaceId == 16044264830 then -- Vazio
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/bladeballfix.lua"))()
+    loadstring(game:HttpGet("b"))()
 end
 -- print placeid
 print(PlaceId)
