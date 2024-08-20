b7921582a102283c44edf203060ddd25a43fc8aa
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-08-20 11:20:17 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -8,8 +8,6 @@
 ::: ##:::: ##:::. ##:. #######:: ########:::: ##:::: ##:::: ##:::: ##:::::::: ########: ##:::. ##:::: ##:::: ##:. #######:: ########::
 :::..:::::..:::::..:::.......:::........:::::..:::::..:::::..:::::..:::::::::........::..:::::..:::::..:::::..:::.......:::........:::
 ]]
--- ui loader/lib loader 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/uilib.txt"))()
 -- ant afk
     game:GetService("Players").LocalPlayer.Idled:connect(function()
      	game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
@@ -20,6 +18,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/
 -- copy
 toclipboard("https://www.discord.gg/v8HuUR9dP8") -- or use dsc.gg/hyperscript
 -- games
+if not _G.hyperloaded then
 local PlaceId = game.PlaceId
 if PlaceId == 13772394625 then -- Blade ball
     repeat wait() until game:IsLoaded()
@@ -38,5 +37,9 @@ elseif PlaceId == 7449423635 then -- Blox s3
 elseif PlaceId == 16044264830 then -- Vazio
     loadstring(game:HttpGet("b"))()
 end
--- print placeid
-print(PlaceId)
+_G.hyperloaded = true
+-- ui loader/lib loader 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyper_Hub/main/uilib.txt"))()
+else 
+    print("loaded")
+end
